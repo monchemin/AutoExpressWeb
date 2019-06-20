01d7a031afe0b4f523a360f82bff33c7c11cdbe0
nyemo <Geoleduc>
2019-06-19 23:01:13 -0400

    Open reservation on right sidebar

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 //http://api-test.toncopilote.com/ 
 //http://localhost/autoexpress-api/
 const Config = {
-    API_HOST: "http://api-test.toncopilote.com/",
-   // API_HOST: "http://localhost/autoexpress-api/",
+   // API_HOST: "http://api-test.toncopilote.com/",
+    API_HOST: "http://localhost/autoexpress-api/",
     HEADER: {headers: { 'Content-Type': 'text/plain' },
     MAXREDIRECT: {maxRedirects: 'auto'}
  }
